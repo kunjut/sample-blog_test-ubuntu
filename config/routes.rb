712244966f2a0d0493febfcdf34976bd4cea6d33
KUNJUT <v.dimnich@gmail.com>
2019-09-27 00:14:01 +0300

    create about page (controller, route, view)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get '/terms', to: 'terms#show'
+  get '/about', to: 'about#show'
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
