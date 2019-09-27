f496ac05641d9ce2dbf78d186bc4050e579ff756
kunjut <v.dimnich@gmail.com>
2019-09-28 01:07:38 +0300

    correct after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_165304) do
+ActiveRecord::Schema.define(version: 2019_09_27_214651) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "email"
