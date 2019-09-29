c558b66f2a34be0c6901c64a0033845e6e3918b8
kunjut <v.dimnich@gmail.com>
2019-09-29 17:58:32 +0300

    article display settings

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,11 @@ class ArticlesController < ApplicationController
 
   def new
   end
-  
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new params_article
     @article.save
