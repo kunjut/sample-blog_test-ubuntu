50324209d858dda2aa88c3cf80a25f22f6a6c2b5
kunjut <v.dimnich@gmail.com>
2019-10-02 01:57:12 +0300

    refactoring edit & update func

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,24 +4,22 @@ class ArticlesController < ApplicationController
     @articles = Article.order 'id DESC'
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def edit
     @article = Article.find(params[:id])
   end
 
-  def update
-    
+  def update   
     @article = Article.find(params[:id])    # запрос в БД записи для Article совпадающей по :id из маршрута
  
-    @article.update(                        # обновление @article
-      title: params[:article][:title],      # передача хэша с актуальными параметрами
-      content: params[:article][:content]
-    )
-    
-    redirect_to article_path                # перенаправление после обновления на /article/:id
-  end
-
-  def show
-    @article = Article.find(params[:id])
+    if @article.update(params_article)      # обновление @article
+      redirect_to @article                  # перенаправление после обновления на /article/:id
+    else
+      render action: 'edit'
+    end
   end
 
   def new
@@ -30,8 +28,7 @@ class ArticlesController < ApplicationController
   def create                                # создание новой записи: create и params_article 
     @article = Article.new params_article   # создание в памяти, записи для Article (c разрешением)
     
-    if @article.valid?                      # если валидация пройдена
-      @article.save                         # сохранение записи в БД
+    if @article.save                        # сохранилось? значит валид-я пройдена, .save сам запускает ее
       redirect_to @article                  # редирект на '/articles' #show
     else                                    
       render action: 'new'                  # иначе вывод экшена 'new' (его вида)
